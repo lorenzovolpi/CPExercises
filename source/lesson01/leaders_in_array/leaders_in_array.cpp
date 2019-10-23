3f20a82c55069828c4698481aead7e47e564b3ac
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2019-10-23 20:45:56 +0200

    Update leaders_in_array.cpp

--- source/lesson01/leaders_in_array/leaders_in_array.cpp
+++ source/lesson01/leaders_in_array/leaders_in_array.cpp
@@ -1,22 +1,51 @@
 #include <iostream>
 #include <vector>
 
+
+void print_leaders_in_array(std::vector<int> vect) {
+
+	std::vector<int> leaders;
+	leaders.reserve(vect.size());
+
+	int max = *vect.rbegin();
+
+	for (auto it = vect.rbegin(); it != vect.rend(); ++it) {
+		if (*it >= max) {
+			max = *it;
+			leaders.push_back(max);
+		}
+	}
+
+	for (auto it = leaders.rbegin(); it != leaders.rend(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << "\n";
+}
+
+
 int main() {
 
 	std::vector<int> vec;
+	int test_cases = 0;
 
-	int n = 0;
-	std::cin >> n;
-	vec.reserve(n);
+	std::cin >> test_cases;
+	
+	for (int i = 0; i < test_cases; ++i) 
+	{
+		int n = 0;
+		std::cin >> n;
+		vec.reserve(n);
 
-	for (int i = 0; i < n; ++i) {
-		int x = 0;
-		std::cin >> x;
-		vec.push_back(x);
-	}
+		for (int i = 0; i < n; ++i) 
+		{
+			int x = 0;
+			std::cin >> x;
+			vec.push_back(x);
+		}
 
-	for (auto it = vec.rbegin(); it != vec.rend(); ++it) {
-		std::cout << *it << " ";
+		print_leaders_in_array(vec);
+
+		vec.clear();
 	}
 
 	return 0;
