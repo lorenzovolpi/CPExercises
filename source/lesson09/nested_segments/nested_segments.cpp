a2884b40e27a10c248bd90c232f918e32c77cc19
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2019-11-04 19:53:15 +0100

    Create nested_segments.cpp

---
+++ source/lesson09/nested_segments/nested_segments.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "../../../lib/fenwick_tree.h"
+
+struct query
+{
+	int f, l, i, n = 0;
+
+	query(int first, int last, int index) : f(first), l(last), i(index) {};
+};
+
+
+int main()
+{
+	int q = 0;
+	std::cin >> q;
+	std::vector<query> qs;
+
+	int max = INT_MIN, min = INT_MAX;
+	for (int i = 0; i < q; ++i)
+	{
+		int f, l;
+		std::cin >> f;
+		std::cin >> l;
+		--f, --l;
+		if (f < min) min = f;
+		if (l > max) max = l;
+		qs.emplace_back(f, l, i);
+	}
+
+	int delta = 0;
+	if (min < 0) delta = 0 - min;
+
+	std::sort(qs.begin(), qs.end(), [](query q1, query q2)
+		{
+			return q1.f > q2.f;
+		});
+
+	fenwick_tree ft(max + delta + 1);
+
+	for (int i = 0; i < q; ++i)
+	{
+		qs[i].n = ft.sum(qs[i].l + delta);
+		ft.add(qs[i].l + delta, 1);
+	}
+
+	std::sort(qs.begin(), qs.end(), [](query q1, query q2)
+		{
+			return q1.i < q2.i;
+		});
+
+	for (int i = 0; i < q; ++i)
+	{
+		std::cout << qs[i].n << std::endl;
+	}
+}
+
