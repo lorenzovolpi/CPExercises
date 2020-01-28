24e78f248ac3fc8672552b78c3512f2edebf3fde
lorenzovolpi <yoyopro@gmail.com>
2020-01-28 18:50:23 +0100

    Update solutions

--- exe/binary_number/binary_number.cpp
+++ exe/binary_number/binary_number.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <vector>
 
+int main() {
+    std::string s;
+    std::cin >> s;
+
+    std::vector<int> d(s.length() + 1, 0);
+    for(int i = 0; i<s.length(); ++i) d[i + 1] = (int)(s[i] - '0');
+
+    int sum = 0;
+    for(int i = d.size() - 1; i>0; ) {
+        if(i == 1) {
+            if(d[i] == 0) sum++;
+            --i; 
+        }
+        else {
+            int oldi = i;
+            while(d[i] == 1) --i;
+
+            if(i == oldi) {
+                sum++;
+                --i;
+            }
+            else {
+                sum += (oldi - i) + 1;
+                d[i] = 1;
+            }
+        }
+    }    
+
+    std::cout << sum << std::endl;
+}
