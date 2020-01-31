5d91f8a10f07a9e66fc7a8a9d61be7820cf74ba1
lorenzovolpi <yoyopro@gmail.com>
2020-01-31 14:06:57 +0100

    Update solutions

--- source/lesson17/longest_increasing_subsequence/longest_increasing_subsequence.cpp
+++ source/lesson17/longest_increasing_subsequence/longest_increasing_subsequence.cpp
@@ -2,23 +2,36 @@
 #include <vector>
 #include <algorithm>
 
+/*
+    This problm is solved using dynamic programming. The input array is scanned
+    fron left to right and a support list is kept, initially empty. For each element
+    of the input array, if it is grater than the last element of the support list, the
+    element is added to the end, otherwise we can we can use binary search to find
+    the smallest element in the list greater than the current element and replace the former
+    with the latter. The LIS is the length of the support list. This solutions runs in
+    O(nlogn) in time.
+*/
+
+int bs(const std::vector<int> &v, int s, int e, int x) {
+    int mid = (s + e) / 2;
+    if(s == e) return s;
+    if(v[mid] >= x) return bs(v, s, mid, x);
+    else return bs(v, mid + 1, e, x);
+}
+
 int lis(const std::vector<int>& items) {
-    std::vector<int> subs(items.size(), 1);
-    int max = 1;
-    for(int i = 1; i < subs.size(); ++i) {
-        int imax = 0;
-
-        for(int j = i-1; j>=0; --j) {
-            if(items[j] < items[i]) {
-                if(subs[j] > imax) imax = subs[j];
-            }
+    std::vector<int> dl(1);
+    dl[0] = items[0];
+    for(int i = 1; i<items.size(); ++i) {
+        if(items[i] > dl.back()) {
+            dl.push_back(items[i]);
+        } else {
+            int r = bs(dl, 0, dl.size() - 1, items[i]);
+            dl[r] = items[i];
         }
-
-        subs[i] = imax + 1;
-        if(subs[i] > max) max = subs[i];
     }
 
-    return max;
+    return dl.size();
 }
 
 int main() {
